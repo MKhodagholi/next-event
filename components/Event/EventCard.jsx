08c30f64b35a6ba569649280d853cbff44760253
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-27 11:43:04 +0330

    9. use Button Component in EventCard Component

--- components/Event/EventCard.jsx
+++ components/Event/EventCard.jsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Button from "../UI/Button";
+
 import moment from "moment/moment";
 
 export default function EventCard({ event }) {
   const { id, image, title, date, address } = event;
 
   return (
-    <div className="flex min-w-[500px] min-h-[200px] bg-white rounded-md shadow-lg overflow-hidden">
-      <div className="min-w-[200px] w-[40%]">
-        <Image src={image} alt="" className="h-full" />
+    <div className="flex bg-white rounded-md shadow-lg overflow-hidden">
+      <div className="w-[40%]">
+        <Image src={image} alt="" className="w-full h-full" />
       </div>
       <div className="flex flex-col justify-between w-[60%] p-4">
         <h2 className="text-2xl mb-4 text-black-900">{title}</h2>
         <div className="text-black-700">{moment(date).format("MMM Do YY")}</div>
         <address className="max-w-[200px] text-black-600">{address}</address>
-        <button className="inline-block mt-4 p-2 rounded-md bg-teal-500 text-white self-end">
+        <Button className="self-end">
           <Link href={`/events/${id}`}>Explore Event</Link>
-        </button>
+        </Button>
       </div>
     </div>
   );
