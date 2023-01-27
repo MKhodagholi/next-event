0d93fcdea3d6598509634ccaca12c574d72736ec
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-27 09:34:12 +0330

    0. initial Project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      primaryColor: "#01B994",
+      black: {
+        400: "#1d1d1d",
+        600: "#aaa",
+        700: "#888",
+        900: "#222",
+      },
+    },
+  },
+  plugins: [],
+};
