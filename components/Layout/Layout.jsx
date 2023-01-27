6ce159746a8fde36c805156b4a7654544efe11a8
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-27 11:43:37 +0330

    10. make Layout main responsive

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-cyan-100 pb-4">
       <Nav />
-      <main className="flex justify-center w-full mt-20 mx-auto">
+      <main className="flex flex-col justify-center w-[80%] mt-20 mx-auto sm:w-[%75] md:w-[70%] lg:w-[60%] xl:w-[50%]">
         {children}
       </main>
     </div>
