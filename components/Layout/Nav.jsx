9334898e928da23f4eb2432ac1542824a4db2629
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-27 20:24:19 +0330

    18. make better navbar in mobile mode

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="flex justify-between items-end px-20 py-4 text-teal-300 bg-black-400 md:px-40">
+    <nav className="flex justify-between items-end px-10 py-4 text-teal-300 bg-black-400 md:px-40">
       <Link href="/">
         <h1 className="text-4xl">NextEvents</h1>
       </Link>
